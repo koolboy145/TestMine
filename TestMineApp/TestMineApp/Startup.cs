f0d4131fb1d0c4898301b989af08aedac1959599
John Jacob <checkingifthisworks@live.com>
2016-11-15 00:10:03 +0530

    TestMineApp Begin Solution

---
+++ TestMineApp/TestMineApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMineApp.Startup))]
+namespace TestMineApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
